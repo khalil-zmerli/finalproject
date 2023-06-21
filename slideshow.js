f23d8443cf6dcc51b6343b3cb1824aba7e3db589
khalil-zmerli <khalil.zmerli@gmail.com>
2023-06-21 13:37:01 +0100

    finalproject

---
+++ slideshow.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.querySelectorAll('.slideshow img');
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
+  }
+  slides[slideIndex].classList.add('active');
+}
